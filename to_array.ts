d516d320607bdd1e99f490a9b314fc5472906027
KnorpelSenf <shtrog@gmail.com>
2024-02-20 18:39:31 +0100

    reduce mem usage of to_array

--- to_array.ts
+++ to_array.ts
@@ -1,6 +1,7 @@
 export function makeToArray<E>(source: Iterable<Promise<IteratorResult<E>>>) {
   return async (buffer?: E[]): Promise<E[]> => {
     if (buffer === undefined) {
+      // create buffer and push to it
       const array: E[] = [];
       for await (const element of source) {
         if (element.done) break;
@@ -8,12 +9,12 @@ export function makeToArray<E>(source: Iterable<Promise<IteratorResult<E>>>) {
       }
       return array;
     }
+
+    // fill existing buffer
     const itr = source[Symbol.iterator]();
     const len = buffer.length;
-    const tasks = Array<IteratorResult<Promise<IteratorResult<E>>>>(len);
-    for (let i = 0; i < len; i++) tasks[i] = itr.next();
     for (let i = 0; i < len; i++) {
-      const task = tasks[i];
+      const task = itr.next();
       if (task.done) break;
       const result = await task.value;
       if (result.done) break;
