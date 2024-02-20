0e8cfc22f2edf24a09b4eb91aafcbcd85f39c4a7
KnorpelSenf <shtrog@gmail.com>
2024-02-20 19:44:34 +0100

    re-add batch

--- batch.ts
+++ batch.ts
@@ -1,20 +1,43 @@
 export function makeBatch<E>(source: Iterable<Promise<IteratorResult<E>>>) {
-  return (count: number): AsyncIterable<E[]> => {
-    async function* collect() {
-      const itr = source[Symbol.asyncIterator]();
-      let result: IteratorResult<E> = await itr.next();
-      while (!result.done) {
-        const tuple = Array(count);
-        for (
-          let i = 0;
-          i < count && !result.done;
-          i++, result = await itr.next()
-        ) {
-          tuple[i] = result.value;
-        }
-        yield tuple;
-      }
-    }
-    return collect();
+  return (count: number): Iterable<Promise<IteratorResult<E[]>>> => {
+    return {
+      [Symbol.iterator]() {
+        const itr = source[Symbol.iterator]();
+        let complete = false;
+        return {
+          next() {
+            if (complete) return { done: true, value: undefined };
+            const promises = Array<Promise<IteratorResult<E>>>(count);
+            let tupleSize = 0;
+            for (; tupleSize < count; tupleSize++) {
+              const res = itr.next();
+              if (res.done) {
+                promises.length = tupleSize;
+                complete = true;
+                break;
+              }
+              promises[tupleSize] = res.value;
+            }
+            return {
+              done: false,
+              value: Promise.all(promises).then((elems) => {
+                const tuple = Array<E>(tupleSize);
+                for (let i = 0; i < tupleSize; i++) {
+                  const elem = elems[i];
+                  if (elem.done) {
+                    tuple.length = i;
+                    break;
+                  }
+                  tuple[i] = elem.value;
+                }
+                return tuple.length === 0
+                  ? { done: true, value: undefined }
+                  : { done: false, value: tuple };
+              }),
+            };
+          },
+        };
+      },
+    };
   };
 }
