fbbc40cb21b43417591010bd9faff2f811dfddec
KnorpelSenf <shtrog@gmail.com>
2024-02-20 18:30:24 +0100

    fix append

--- append.ts
+++ append.ts
@@ -2,10 +2,26 @@ export function makeAppend<E>(source: Iterable<Promise<IteratorResult<E>>>) {
   return (
     ...others: Iterable<Promise<IteratorResult<E>>>[]
   ): Iterable<Promise<IteratorResult<E>>> => {
-    function* append() {
-      yield* source;
-      for (const other of others) yield* other;
-    }
-    return append();
+    return {
+      [Symbol.iterator]() {
+        const its = [source, ...others].map((it) => it[Symbol.iterator]());
+        let i = 0;
+        async function nextElem(): Promise<IteratorResult<E>> {
+          for (; i < its.length; i++) {
+            const it = its[i].next();
+            if (it.done) continue;
+            const elem = await it.value;
+            if (elem.done) continue;
+            return elem;
+          }
+          return { done: true, value: undefined };
+        }
+        return {
+          next() {
+            return { done: false, value: nextElem() };
+          },
+        };
+      },
+    };
   };
 }
