84c02e7912d6ac333bb139e52ad53aa357afef6b
KnorpelSenf <shtrog@gmail.com>
2024-02-20 18:39:41 +0100

    early stop zip

--- zip.ts
+++ zip.ts
@@ -6,16 +6,17 @@ export function makeZip<E>(source: Iterable<Promise<IteratorResult<E>>>) {
       [Symbol.iterator]() {
         const left = source[Symbol.iterator]();
         const right = other[Symbol.iterator]();
-        async function both(): Promise<IteratorResult<[E, T]>> {
-          const [l, r] = [left.next(), right.next()];
-          if (l.done || r.done) return { done: true, value: undefined };
-          const [lv, rv] = await Promise.all([l.value, r.value]);
-          if (lv.done || rv.done) return { done: true, value: undefined };
-          return { done: false, value: [lv.value, rv.value] };
-        }
         return {
           next() {
-            return { done: false, value: both() };
+            const [l, r] = [left.next(), right.next()];
+            if (l.done || r.done) return { done: true, value: undefined };
+            return {
+              done: false,
+              value: Promise.all([l.value, r.value]).then(([lv, rv]) => {
+                if (lv.done || rv.done) return { done: true, value: undefined };
+                return { done: false, value: [lv.value, rv.value] };
+              }),
+            };
           },
         };
       },
