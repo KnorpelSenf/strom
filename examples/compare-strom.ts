c07189d7243a9855aa5767521d53a2c5dfa3e984
KnorpelSenf <shtrog@gmail.com>
2024-02-20 14:04:18 +0100

    add fetch example

---
+++ examples/compare-strom.ts
@@ -0,0 +1,31 @@
+import { strom } from "../mod.ts";
+
+const urls = [
+  { url: "https://github.com" },
+  { url: "https://google.com" },
+  { url: "https://wikipedia.org" },
+  { url: "https://github.com" },
+  { url: "https://google.com" },
+  { url: "https://wikipedia.org" },
+  { url: "https://github.com" },
+  { url: "https://google.com" },
+  { url: "https://wikipedia.org" },
+  { url: "https://github.com" },
+  { url: "https://google.com" },
+  { url: "https://wikipedia.org" },
+  { url: "https://github.com" },
+  { url: "https://google.com" },
+  { url: "https://wikipedia.org" },
+  { url: "https://github.com" },
+  { url: "https://google.com" },
+  { url: "https://wikipedia.org" },
+  { url: "https://github.com" },
+  { url: "https://google.com" },
+];
+console.time();
+await strom(urls)
+  .map(({ url }) => fetch(url))
+  .buffer()
+  .run()
+  .task();
+console.timeEnd();
