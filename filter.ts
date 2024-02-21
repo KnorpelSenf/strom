ab2d3cf2909a0f9738ebd534174998c29c89fddc
KnorpelSenf <shtrog@gmail.com>
2024-02-21 23:34:53 +0100

    supply index to filter predicate

--- filter.ts
+++ filter.ts
@@ -5,26 +5,31 @@ type Option<E> = { ok: false } | { ok: true; some: E };
 
 export function makeFilter<E>(source: Iterable<Promise<IteratorResult<E>>>) {
   return (
-    predicate: (element: E) => boolean | Promise<boolean> = (e) => e != null,
+    predicate: (element: E, index: number) => boolean | Promise<boolean> = (
+      e,
+    ) => e != null,
   ): Iterable<Promise<IteratorResult<E>>> => {
-    async function test(val: IteratorResult<E>) {
-      if (val.done) return val;
-      const okValue: Option<E> = await predicate(val.value)
-        ? { ok: true, some: val.value }
-        : { ok: false };
-      return { done: false, value: okValue };
-    }
-
     return {
       [Symbol.iterator]() {
         const it = source[Symbol.iterator]();
+        let index = 0;
         const values = empty<Promise<IteratorResult<Option<E>>>>();
         const consumers = empty<Deferred<void>>();
         return {
           next() {
+            const i = index++;
             // eagerly fetch and test the next element, enqueue it
             const res = it.next();
             if (res.done) return res;
+
+            async function test(val: IteratorResult<E>) {
+              if (val.done) return val;
+              const okValue: Option<E> = await predicate(val.value, i)
+                ? { ok: true, some: val.value }
+                : { ok: false };
+              return { done: false, value: okValue };
+            }
+
             enqueue(values, res.value.then(test));
             // concurrently wait for it to arrive unless we are first
             const resume = deferred();
