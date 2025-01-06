d21783258007839d781daf83824bf2267373ba15
Steffen Trog <steffen.trog@fastleansmart.com>
2025-01-06 16:49:25 +0100

    feat: drop flat_map

--- flat_map.ts
+++ flat_map.ts
@@ -1,48 +1,10 @@
-export function makeFlatMap<E>(source: Iterable<Promise<IteratorResult<E>>>) {
+export function makeFlatMap<E>(_source: Iterable<Promise<IteratorResult<E>>>) {
   return <T>(
-    transform: (
+    _transform: (
       element: E,
       index: number,
     ) => Iterable<Promise<IteratorResult<T>>>,
   ): Iterable<Promise<IteratorResult<T>>> => {
-    return {
-      [Symbol.iterator]() {
-        const it = source[Symbol.iterator]();
-        let index = 0;
-        async function nextIter(): Promise<
-          IteratorResult<Iterator<Promise<IteratorResult<T>>>>
-        > {
-          const cur = it.next();
-          if (cur.done) return cur;
-          const res = await cur.value;
-          if (res.done) return res;
-          const elem = res.value;
-          const nested = transform(elem, index++);
-          return { done: false, value: nested[Symbol.iterator]() };
-        }
-        let last:
-          | IteratorResult<Iterator<Promise<IteratorResult<T>>>>
-          | undefined;
-        async function nextElem(): Promise<IteratorResult<T>> {
-          while (!(last ??= await nextIter()).done) {
-            const elem = last.value.next();
-            if (!elem.done) {
-              const val = await elem.value;
-              if (!val.done) {
-                return val;
-              }
-            }
-            // current iterator is exhausted, reset
-            last = undefined;
-          }
-          return { done: true, value: undefined };
-        }
-        return {
-          next() {
-            return { done: false, value: nextElem() };
-          },
-        };
-      },
-    };
+    throw new Error("flatmap not implemented, please open a pull request");
   };
 }
